9f68dfb2c7a22be89f7366dfff57b4ad35f3bce4
chandralekathangaraj123 <chandru0893@gmail.com>
2022-08-22 14:42:18 +0530

    New code

--- src/Looping.java
+++ src/Looping.java
@@ -4,9 +4,12 @@ public class Looping {
 	 int age=10;
 	 int studentId=25897;
 	 if (age==10 && studentId==25897 )
-		 System.out.println("true");
+		 System.out.println("chandraleka");
+// System.out.println("Executed succefully");
 	 else
+	 {
 		 System.out.println("False");
+	 }
 	}
 
 }
