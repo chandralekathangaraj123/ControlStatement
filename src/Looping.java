17d44b041def8066e67c198bf7064528d788649b
chandralekathangaraj123 <chandru0893@gmail.com>
2022-08-22 14:48:54 +0530

    Changed code

--- src/Looping.java
+++ src/Looping.java
@@ -4,7 +4,7 @@ public class Looping {
 	 int age=10;
 	 int studentId=25897;
 	 if (age==10 && studentId==25897 )
-		 System.out.println("true");
+		 System.out.println("Raj");
 	 else
 		 System.out.println("False");
 	}
